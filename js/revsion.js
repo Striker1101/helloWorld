7ad5392f22c1236b1b63cfdf4adbc7363e58fae8
striker1101 <goodluckodemilin@gmail.com>
2024-07-10 12:23:46 +0100

    july 10 revision

---
+++ js/revsion.js
@@ -0,0 +1,7 @@
+function testOne(numOne, numTwo) {
+  const result = numOne / numTwo;
+  console.log(result);
+  return result;
+}
+
+module.exports = testOne;
---
+++ test/revision.test.js
@@ -0,0 +1,6 @@
+const testOne = require("../js/revsion");
+
+test("should divide two numbers", () => {
+  //write your test
+  expect(testOne(45, 5)).toBe(9);
+});
